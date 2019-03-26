d6108be93d8e79e9b02fa6717fdce52d3208e1db
guangyangai <guang.k.yang@gmail.com>
2019-03-26 17:24:53 +0000

    first stab of tl classifier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,21 @@
+import rospy
+import cv2
+import time
+import numpy as np
+import tensorflow as tf
+from keras import backend
+from keras import layers
+from keras.models import load_model
+from keras.applications.vgg16 import preprocess_input
 from styx_msgs.msg import TrafficLight
 
 class TLClassifier(object):
-    def __init__(self):
+    def __init__(self, is_site):
         #TODO load classifier
-        pass
+        if is_site:
+            self.model = load_model('../../../data_science/models/site_model.h5')
+        else:
+            self.model = load_model('../../../data_science/models/simulator_model.h5')
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -16,4 +28,5 @@ class TLClassifier(object):
 
         """
         #TODO implement light color prediction
+        # We know the location of the light 
         return TrafficLight.UNKNOWN
